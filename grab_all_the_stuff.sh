854a85617e0b1a52ce6fb7588289f67d793d0055
Joshua <joshua@guardian.ag>
2023-06-13 10:56:59 -0400

    initial

---
+++ grab_all_the_stuff.sh
@@ -0,0 +1,4 @@
+cp ~/.bashrc .
+cp ~/.bash_aliases .
+cp ~/.config/tmux/tmux.conf ./.config/tmux/tmux.conf
+cp -r ~/.config/nvim/* ./.config/nvim/
---
+++ setup_script.sh
@@ -0,0 +1,9 @@
+# nvim nvchad
+# tmux
+# tmux vim bindings
+#
+
+sudo apt install xcape
+setxkbmap -option 'caps:escape'
+
+
