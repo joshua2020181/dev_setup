fe700ffb24d7761f5bc00c5a94e7dfc1caafbc80
Joshua Cheng <josh.cheng.sh@gmail.com>
2023-06-26 11:00:15 -0400

    move lua stuff

--- grab_all_the_stuff.sh
+++ grab_all_the_stuff.sh
@@ -4,4 +4,4 @@ mkdir -p .config/tmux
 mkdir -p .config/nvim/lua
 cp ~/.config/tmux/tmux.conf .config/tmux/tmux.conf
 cp ~/.config/nvim/init.lua .config/nvim/
-cp -r ~/.config/nvim/lua/ .config/nvim/lua
+cp -r ~/.config/nvim/lua/ .config/nvim/
