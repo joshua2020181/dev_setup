11a2768e614a43e7233e9bebaf3cd88373966777
Joshua Cheng <josh.cheng.sh@gmail.com>
2023-06-28 09:38:09 -0400

    tpm and keyboard

--- setup_script.sh
+++ setup_script.sh
@@ -25,7 +25,9 @@ git clone https://github.com/NvChad/NvChad ~/.config/nvim --depth 1
 
 # smart caps lock (caps lock is escape when tapped, control when held)
 sudo apt install xcape
-setxkbmap -option 'caps:escape'
+setxkbmap -option  # clear all options
+setxkbmap -option 'ctrl:nocaps'
+xcape -e 'Control_L=Escape'  # caps lock is escape when tapped
 # maybe this honestly i forgot: https://gist.github.com/tanyuan/55bca522bf50363ae4573d4bdcf06e2e
 
 sudo apt install xsel  # for clipboard support in tmux
@@ -37,4 +39,4 @@ sudo apt install xsel  # for clipboard support in tmux
 cp .config/tmux/tmux.conf ~/.config/tmux/tmux.conf
 cp .config/nvim/init.lua ~/.config/nvim/init.lua
 cp -r .config/nvim/lua ~/.config/nvim/
-
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm  # tmux plugin manager
