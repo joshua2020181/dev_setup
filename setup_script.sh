8ae8a784b4725f512d28d600da48a20bf1dbe10e
Joshua Cheng <josh.cheng.sh@gmail.com>
2023-07-10 15:14:23 -0400

    move gh auth to initial

--- setup_script.sh
+++ setup_script.sh
@@ -1,24 +1,6 @@
 sudo apt update && sudo apt upgrade -y
 sudo apt install -y tmux neovim python3 python3-pip curl wget
 
-# install gh command line
-type -p curl >/dev/null || (sudo apt update && sudo apt install curl -y)
-curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg \
-&& sudo chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg \
-&& echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
-&& sudo apt update \
-&& sudo apt install gh -y
-
-# gh CLI login
-gh auth login
-
-# setup ssh key for github
-if [ ! -f "~/.ssh/id_ed25519"]; then
-  ssh-keygen -t ed25519 -C "josh.cheng.sh@gmail.com" -f ~/.ssh/id_ed25519 -N ""
-  eval "$(ssh-agent -s)"
-  ssh-add ~/.ssh/id_ed25519
-  gh ssh-key add ~/.ssh/id_ed25519.pub
-fi
 
 # install nvchad
 git clone https://github.com/NvChad/NvChad ~/.config/nvim --depth 1
